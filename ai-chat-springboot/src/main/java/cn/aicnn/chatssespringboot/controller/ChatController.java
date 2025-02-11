2398395527f52f810c7ad98f2bc286804ff3cffe
fangdudu <1042096700@qq.com>
2025-02-11 15:48:09 +0800

    init

---
+++ ai-chat-springboot/src/main/java/cn/aicnn/chatssespringboot/controller/ChatController.java
@@ -0,0 +1,34 @@
+package cn.aicnn.chatssespringboot.controller;
+
+import cn.aicnn.chatssespringboot.dto.AIAnswerDTO;
+import cn.aicnn.chatssespringboot.service.GptServiceImpl;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+import javax.annotation.Resource;
+
+
+@RestController
+public class ChatController {
+
+    //用于流式请求第三方的实现类
+    @Resource
+    GptServiceImpl gptService;
+
+    //通过stream返回流式数据
+    @GetMapping(value = "/completions", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<AIAnswerDTO>> getStream(@RequestParam("messages")String messages) {
+        //实现类发送消息并获取返回结果
+        return gptService.doChatGPTStream(messages)
+                //进行结果的封装，再返回给前端
+                .map(aiAnswerDTO -> ServerSentEvent.<AIAnswerDTO>builder()
+                        .data(aiAnswerDTO)
+                        .build()
+                )
+                //发生异常时发送空对象
+                .onErrorResume(e -> Flux.empty());
+    }
+
+}
---
+++ ai-chat-springboot/src/main/java/cn/aicnn/chatssespringboot/dto/AIAnswerDTO.java
@@ -0,0 +1,98 @@
+package cn.aicnn.chatssespringboot.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AIAnswerDTO {
+    /**
+     * 对话id 例如：chatcmpl-c16bf13f-d55a-93c6-9f27-258cc24805e2
+     */
+    private String id;
+    /**
+     * 聊天object 例如：chat.completion.chunk
+     */
+    private String object;
+    /**
+     * 创建时间戳 例如：1739256224
+     */
+    private int created;
+    /**
+     * 使用的模型 例如：deepseek-r1
+     */
+    private String model;
+    /**
+     * 系统指纹
+     */
+    public String system_fingerprint;
+    /***
+     * 消耗的token相关信息
+     */
+    private AIAnswerUsageDTO usage;
+    /**
+     * 回答内容
+     */
+    private List<ChoicesDTO> choices = new ArrayList<>();
+    /**
+     * 回答的内容
+     */
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class ChoicesDTO {
+        /**
+         * 数据索引
+         */
+        public int index;
+        /**
+         * 角色以及回答的内容
+         */
+        public Delta delta;
+        /**
+         * 完成原因
+         */
+        private String finish_reason;
+        /**
+         * probs
+         */
+        private String logprobs;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Delta {
+        /**
+         * 角色
+         */
+        public String role;
+        /**
+         * 回复内容
+         */
+        public String content = "";
+        /**
+         * 思考过程
+         */
+        public String reasoning_content = "";
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class AIAnswerUsageDTO {
+        /**
+         * 提示词消耗的token数
+         */
+        public int prompt_tokens;
+        /**
+         * 对话消耗的token数
+         */
+        public int completion_tokens;
+        /**
+         * 总token数
+         */
+        public int total_tokens;
+    }
+}
