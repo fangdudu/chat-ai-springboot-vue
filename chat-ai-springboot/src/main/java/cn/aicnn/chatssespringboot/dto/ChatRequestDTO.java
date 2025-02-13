bcca8b336231f6d1fb38e3734933db890ab8e845
fangdudu <1042096700@qq.com>
2025-02-13 11:47:03 +0800

    文件名修改

---
+++ chat-ai-springboot/src/main/java/cn/aicnn/chatssespringboot/dto/ChatRequestDTO.java
@@ -0,0 +1,29 @@
+package cn.aicnn.chatssespringboot.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+public class ChatRequestDTO {
+    private String model;
+
+    private List<ReqMessage> messages;
+
+    private Boolean stream;
+
+    private StreamOptions stream_options;
+
+    @Data
+    public static class ReqMessage {
+        private String role;
+        private String content;
+    }
+
+    @Data
+    public static class StreamOptions {
+        private Boolean include_usage;
+    }
+
+}
